0efd3e0c82d3e34f7be66cbd0c1c23ad829de80c
Tanjilur <trmtanjil02@gmail.com>
2025-08-11 17:14:00 +0600

    navbar satup  commit

--- src/Page/Components/Navbar.jsx
+++ src/Page/Components/Navbar.jsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> PedroTech </span>{" "}
+            <span className="text-glow text-foreground"> Tanjilur </span>{" "}
             Portfolio
           </span>
         </a>
